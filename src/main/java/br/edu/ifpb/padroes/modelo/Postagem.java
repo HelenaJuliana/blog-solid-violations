18907b26afc19e6851398722dd2adaec0d3bfd16
Helena <helenajuliana14@gmail.com>
2021-02-12 18:28:20 -0300

    SOLID

--- src/main/java/br/edu/ifpb/padroes/modelo/Postagem.java
+++ src/main/java/br/edu/ifpb/padroes/modelo/Postagem.java
@@ -1,56 +1,74 @@
 package br.edu.ifpb.padroes.modelo;
 
 public class Postagem {
-
-    public enum PostagemTipo { PUBLICA, PRIVADA }
-
-    private Long id;
-
-    private String titulo;
-
-    protected Usuario usuario;
-
-    private String mensagem;
-
-    private PostagemTipo postagemTipo;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getMensagem() {
-        return mensagem;
-    }
-
-    public void setMensagem(String mensagem) {
-        this.mensagem = mensagem;
-    }
-
-    public PostagemTipo getPostagemTipo() {
-        return postagemTipo;
-    }
-
-    public void setPostagemTipo(PostagemTipo postagemTipo) {
-        this.postagemTipo = postagemTipo;
-    }
+ 
+   public enum PostagemTipo { PUBLICA, PRIVADA }
+ 
+   private Long id;
+ 
+   private String titulo;
+ 
+   protected Usuario usuario;
+ 
+   private String mensagem;
+ 
+   private PostagemTipo postagemTipo;
+ 
+ 
+public static class  ExtendPostagem {
+ 
+   public static String TipoPostagem  (postagem ){
+ 
+       if ( postagem == publica ){
+           System.out.println(“Postagem  Pública.”)
+ 
+       }else if{
+           System.out.println(“Postagem  Privada.”)
+ 
+        
+   }
+ 
+}
+ 
+   public Long getId() {
+       return id;
+   }
+ 
+   public void setId(Long id) {
+       this.id = id;
+   }
+ 
+   public String getTitulo() {
+       return titulo;
+   }
+ 
+   public void setTitulo(String titulo) {
+       this.titulo = titulo;
+   }
+ 
+   public Usuario getUsuario() {
+       return usuario;
+   }
+ 
+   public void setUsuario(Usuario usuario) {
+       this.usuario = usuario;
+   }
+ 
+   public String getMensagem() {
+       return mensagem;
+   }
+ 
+   public void setMensagem(String mensagem) {
+       this.mensagem = mensagem;
+   }
+ 
+   public PostagemTipo getPostagemTipo() {
+       return postagemTipo;
+   }
+ 
+   public void setPostagemTipo(PostagemTipo postagemTipo) {
+       this.postagemTipo = postagemTipo;
+   }
+ 
+  
 }
--- src/main/java/br/edu/ifpb/padroes/modelo/Usuario.java
+++ src/main/java/br/edu/ifpb/padroes/modelo/Usuario.java
@@ -2,55 +2,78 @@ package br.edu.ifpb.padroes.modelo;
 
 import java.util.List;
 
+
 public class Usuario {
-
-    private Long id;
-
-    private String nome;
-
-    private String login;
-
-    private String senha;
-
-    private List<Postagem> postagens;
-
-    public List<Postagem> getPostagens() {
-        return postagens;
-    }
-
-    public void setPostagens(List<Postagem> postagens) {
-        this.postagens = postagens;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+ 
+   private Long id;
+ 
+   private String nome;
+ 
+   private String login;
+ 
+   private String senha;
+ 
+   private List<Postagem> postagens;
+ 
+ 
+public void Postagem ( String postagem){
+     this.postagem = postagem;
+  
+   }
+ 
+    public void LoginUse ( String senha, String nome){
+               this.senha = senha;
+               this.nome = nome;
+  
+   }
+ 
+   public List<Postagem> getPostagens() {
+       return postagens;
+   }
+ 
+  
+ 
+   public void setPostagens(List<Postagem> postagens) {
+       this.postagens = postagens;
+   }
+ 
+   public Long getId() {
+       return id;
+   }
+ 
+   public void setId(Long id) {
+       this.id = id;
+   }
+ 
+   public String getNome() {
+       return nome;
+   }
+ 
+   public void setNome(String nome) {
+       this.nome = nome;
+   }
+ 
+   public String getLogin() {
+       return login;
+   }
+ 
+   public void setLogin(String login) {
+       this.login = login;
+   }
+ 
+   public String getSenha() {
+       return senha;
+   }
+ 
+   public void setSenha(String senha) {
+       this.senha = senha;
+   }
+ 
+   public PostagemListar getPostagemListar() {
+       return postagemListar;
+   }
+     public Login getLogin() {
+       return login;
+   }
+ 
 }
