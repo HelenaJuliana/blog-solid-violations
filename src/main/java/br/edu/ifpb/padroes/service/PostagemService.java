054ce75702226942f3023c73e6ede79ec17380d2
RafaelMarinhoo <marinhor978@gmail.com>
2021-02-12 19:48:04 -0300

    aplicando o princípio de Segregação de interfaces

--- src/main/java/br/edu/ifpb/padroes/service/PostagemService.java
+++ src/main/java/br/edu/ifpb/padroes/service/PostagemService.java
@@ -2,4 +2,9 @@ package br.edu.ifpb.padroes.service;
 
 public interface PostagemService {
 
+    void adicionarPostagem(Postagem postagem);
+    void removerPostagem(Postagem postagem);
+    void atualizarPostagem(Postagem postagem);
+    void adicionarResposta(Postagem postagem, Postagem resposta);
+
 }
